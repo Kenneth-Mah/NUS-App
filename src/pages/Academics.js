436e6d4f7b15f39aa1689400f033e722c888dcb0
Kenneth-Mah <kennethmah@rocketmail.com>
2023-11-06 13:28:11 +0800

    Completed pages

--- src/pages/Academics.js
+++ src/pages/Academics.js
@@ -1,3 +1,29 @@
 export default function Academics() {
-    return <h1>Academics</h1>
+    return (
+        <>
+            <h1>Academics</h1>
+            <table>
+                <tr>
+                    <th>Module Code</th>
+                    <th>Module Title</th>
+                </tr>
+                <tr>
+                    <td>BT1101</td>
+                    <td>Introduction to Business Analytics</td>
+                </tr>
+                <tr>
+                    <td>BT2101</td>
+                    <td>Econometrics Modelling for Business Analytics</td>
+                </tr>
+                <tr>
+                    <td>BT2102</td>
+                    <td>Data Management and Visualisation</td>
+                </tr>
+                <tr>
+                    <td>BT2103</td>
+                    <td>Optimization Methods in Business Analytics</td>
+                </tr>
+            </table>
+        </>
+    )
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,3 +1,11 @@
+import nus_logo from "../nus_logo_full-vertical.jpg"
+
 export default function Home() {
-    return <h1>Home</h1>
+    return (
+        <>
+            <h1>Home</h1>
+            <img src={nus_logo} width={250} height={330}/>
+            <p>NUS is a leading research university in Asia</p>
+        </>
+    )
 }
